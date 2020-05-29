c193152422b5e188dc13d933675b7338299f3064
Uwe L. Korn <uwe.korn@quantco.com>
2020-05-29 08:33:06 +0200

    Remove object files

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,6 @@ mkdir -p out/release
 cd out/release
 cmake -GNinja -DCMAKE_INSTALL_PREFIX=$PREFIX -DMI_BUILD_TESTS=OFF ../..
 ninja install
-# No static libraries on conda-forge, please.
+# No static libraries or object files on conda-forge, please.
 rm $PREFIX/lib/libmimalloc.a
+rm $PREFIX/lib/mimalloc.o
