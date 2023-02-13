9811af8910aeb50088b5257912f46fbd0d4c3beb
Uwe L. Korn <xhochy@users.noreply.github.com>
2023-02-13 05:32:38 +0100

    Add a compilation test (#6)
    * Add a compilation test
    
    * Set LD_LIBRARY_PATH
    
    * Run tests on Windows
    
    * MNT: Re-rendered with conda-build 3.23.3, conda-smithy 3.22.1, and conda-forge-pinning 2023.02.12.11.51.20
    
    ---------
    
    Co-authored-by: elbaro <elbaro@users.noreply.github.com>
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,11 +7,15 @@ cmake ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DMI_INSTALL_TOPLEVEL=ON ^
-    -DMI_BUILD_TESTS=OFF ^
+    -DMI_BUILD_TESTS=ON ^
     -DMI_BUILD_SHARED=ON  ^
     -DMI_BUILD_STATIC=OFF ^
     -DMI_BUILD_OBJECT=OFF ^
     ..\..
+if errorlevel 1 exit 1
+ninja
+if errorlevel 1 exit 1
+ctest
 if errorlevel 1 exit 1
 ninja install
 if errorlevel 1 exit 1
